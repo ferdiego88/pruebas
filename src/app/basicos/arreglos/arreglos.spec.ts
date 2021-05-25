29bf67a21f617a618e25ae14b35301776fca2c69
Fernando Santillan <ferdiego88@gmail.com>
2021-05-24 23:21:37 -0500

    Probando arreglos

---
+++ src/app/basicos/arreglos/arreglos.spec.ts
@@ -0,0 +1,18 @@
+import { obtenerRobots } from './arreglos';
+describe('Pruebas de Arreglos', () => {
+
+  it('Debe de retornar al menos 3 Robots', () => {
+    const resp = obtenerRobots();
+    // expect(resp.length).toBe(3);
+    expect(resp.length).toBeGreaterThanOrEqual(3);
+  });
+
+  it('Deben de existir Megaman y Ultron', () => {
+
+    const resp = obtenerRobots();
+    expect(resp).toContain('Megaman');
+    expect(resp).toContain('Ultron');
+
+  });
+
+});
